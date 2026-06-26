ada0a43a049d9eb9ebff26a554dda13a33299718
Nokulunga151 <ST10497028@rcconnect.edu.za>
2026-06-26 11:42:42 +0200

    Added JSON task storage with TaskStorageHelper and CyberTask model

--- TaskStorageHelper.cs
+++ TaskStorageHelper.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace BotmanChatbot
 {
     internal class TaskStorageHelper
-    {
+    { 
         public string Title { get; set; }
         public string Reminder { get; set; }
         public bool IsComplete { get; set; }
